3941bdb424804ed11ffe9a6fb57648cee2a0ca39
Julius Gubitz <julius.gubitz@web.de>
2026-03-03 12:54:56 +0100

    260303_password protection disabled

--- lib/password-protection.ts
+++ lib/password-protection.ts
@@ -2,5 +2,5 @@
  * Passwortschutz für Entwicklungs-Testzwecke.
  * Auf false setzen und Datei speichern, um den Schutz vollständig zu entfernen.
  */
-export const PASSWORD_PROTECTION_ENABLED = true;
+export const PASSWORD_PROTECTION_ENABLED = false;
 export const PASSWORD = "123";
